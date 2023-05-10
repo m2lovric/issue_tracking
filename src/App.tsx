ec4ff2e1684c8baa0d43ded4b4d740fb25beffb1
m2lovric <m2lovric@gmail.com>
2023-05-10 13:20:24 +0200

    React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import db from './db.ts';
+import db from './store/db.ts';
 import { collection, onSnapshot } from 'firebase/firestore';
 
 const getData = () => {
@@ -14,35 +11,16 @@ const getData = () => {
 };
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <section>
+        <h2>App</h2>
+      </section>
+    </div>
   );
 }
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <>
+      <form action=''>
+        <input type='text' />
+        <input type='password' name='' id='' />
+      </form>
+    </>
+  );
+};
+
+export default Login;
